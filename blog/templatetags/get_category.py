181e7d93d117bf769fd3e002a70e1bf8576586ad
Chepolina <tv-chesnokova2012@yandex.ru>
2016-03-06 17:51:24 +0300

    minor fixes and pythonanywhere.com integration

--- blog/templatetags/get_category.py
+++ blog/templatetags/get_category.py
@@ -5,8 +5,8 @@ register = Library()
 @register.filter
 def get_category(f):
     return ['popular chocolate bar' ,
-            'hoot chocolate on a spoon',
+            'hot chocolate on a spoon',
             'chocolate sets',
-            'business gift',
+            'business gifts',
             'luxury truffles']
 
