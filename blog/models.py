4e6da92095a07374ddd310a38945d3106442ee24
Polina Chesnokova <tv-chesnokova2012@yandex.ru>
2016-05-30 14:42:37 +0000

    cart

--- blog/models.py
+++ blog/models.py
@@ -11,29 +11,33 @@ class Payment(models.Model):
     payment_type = models.CharField(max_length=200)
 
 class Cart(models.Model):
-    total_cost = models.IntegerField()
-
-
-
-class Customer(models.Model):
-    author = models.ForeignKey('auth.User')
-    email = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    name  = models.CharField(max_length=200)
-    mobile = models.IntegerField()
     payment = models.OneToOneField(Payment, on_delete=models.CASCADE, primary_key=False,)
+    customer = models.ForeignKey("auth.User", default=None)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def total():
+        summ = 0
+        for obj in self.Cart_item:
+            summ += obj.cost
+        return summ
+
+# class Customer(models.Model):
+#     author = models.ForeignKey('auth.User')
+#     email = models.CharField(max_length=200)
+#     address = models.CharField(max_length=200)
+#     name  = models.CharField(max_length=200)
+#     mobile = models.CharField()
+#     payment = models.OneToOneField(Payment, on_delete=models.CASCADE, primary_key=False,)
 
 
 class Product(models.Model):
-    published_date = models.DateTimeField(default=timezone.now, editable=True)
     name = models.CharField(max_length=200)
+    published_date = models.DateTimeField(default=timezone.now, editable=True)
     description = models.TextField()
     cost = models.IntegerField()
     weight = models.FloatField()
     image = models.ImageField(upload_to="%Y/%m/%d/", height_field=None, width_field=None, max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    customer = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-
 
     def publish(self):
         self.published_date = timezone.now()
@@ -42,9 +46,8 @@ class Product(models.Model):
 
 
 class Cart_item(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, primary_key=False,)
     cost = models.IntegerField()
     quantity = models.IntegerField()
-    payment = models.OneToOneField(Payment, on_delete=models.CASCADE, primary_key=False,)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, primary_key=False,)
 
